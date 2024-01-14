19a1f0346a5cbd343327db3ded71c68226d50528
Tristan Sutton <tsutton@ORG5118675.OCN.ORIGIN.COM.AU>
2024-01-15 09:43:20 +1100

    Move plugins to seperate files

---
+++ lua/plugins/lazy.lua
@@ -0,0 +1,15 @@
+return {
+	{
+		"LazyVim/LazyVim",
+		opts = {
+			colorscheme = "catppuccin",
+			defaults = {
+				lazy = true,
+				keymaps = false,
+			},
+			checker = {
+				enabled = true,
+			},
+		},
+	},
+}
