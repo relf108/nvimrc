5e0945451677cc44102658c0e54797b9c27839c8
Tristan Sutton <tsutton@Tristans-MacBook-Pro.local>
2024-01-25 15:27:01 +1100

    trouble

--- lua/plugins/trouble.lua
+++ lua/plugins/trouble.lua
@@ -7,7 +7,7 @@ return {
 				require("trouble").toggle({ mode = "workspace_diagnostics" })
 			end)
 			vim.keymap.set("n", "<leader>xl", function()
-				require("trouble").toggle({ mode = "loclist" })
+				require("trouble").toggle({ mode = "document_diagnostics" })
 			end)
 		end,
 	},
