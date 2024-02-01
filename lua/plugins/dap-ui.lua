85cb0fdcb45f51ac8a04e3923e9c9440dae4f018
Tristan Sutton <tsutton@ORG5124820.OCN.ORIGIN.COM.AU>
2024-02-01 13:08:07 +1100

    Mason dap

---
+++ lua/plugins/dap-ui.lua
@@ -0,0 +1,97 @@
+return {
+	{
+		"rcarriga/nvim-dap-ui",
+		config = function()
+			require("dapui").setup({
+				controls = {
+					element = "repl",
+					enabled = true,
+					icons = {
+						disconnect = "",
+						pause = "",
+						play = "",
+						run_last = "",
+						step_back = "",
+						step_into = "",
+						step_out = "",
+						step_over = "",
+						terminate = "",
+					},
+				},
+				element_mappings = {},
+				expand_lines = true,
+				floating = {
+					border = "single",
+					mappings = {
+						close = { "q", "<Esc>" },
+					},
+				},
+				force_buffers = true,
+				icons = {
+					collapsed = "",
+					current_frame = "",
+					expanded = "",
+				},
+				layouts = {
+					{
+						elements = {
+							{
+								id = "breakpoints",
+								size = 0.15,
+							},
+							{
+								id = "watches",
+								size = 0.15,
+							},
+							{
+								id = "stacks",
+								size = 0.15,
+							},
+							{
+								id = "scopes",
+								size = 0.55,
+							},
+						},
+						position = "left",
+						size = 60,
+					},
+					{
+						elements = {
+							{
+								id = "repl",
+								size = 0.5,
+							},
+							{
+								id = "console",
+								size = 0.5,
+							},
+						},
+						position = "bottom",
+						size = 20,
+					},
+				},
+				mappings = {
+					edit = "e",
+					expand = { "<CR>", "<2-LeftMouse>" },
+					open = "o",
+					remove = "d",
+					repl = "r",
+					toggle = "t",
+				},
+				render = {
+					indent = 1,
+					max_value_lines = 100,
+				},
+			})
+			require("dap").listeners.after.event_initialized["dapui_config"] = function()
+				require("dapui").open()
+			end
+			vim.api.nvim_set_keymap(
+				"n",
+				"<leader>du",
+				':lua require("dapui").toggle()<CR>',
+				{ noremap = true, silent = true }
+			)
+		end,
+	},
+}
--- lua/plugins/mason.lua
+++ lua/plugins/mason.lua
@@ -66,6 +66,7 @@ return {
 					"yamlls",
 					"typos_lsp",
 					"marksman",
+					"bashls",
 				},
 			})
 			require("mason-lspconfig").setup_handlers({
@@ -105,9 +106,9 @@ return {
 					local ruff_conf = vim.fn.expand("~/.config/ruff/ruff.toml")
 					require("lspconfig")[server_name].setup({
 						capabilities = capabilities,
-            on_attach = function(client)
-              client.server_capabilities.hoverProvider = false
-            end,
+						on_attach = function(client)
+							client.server_capabilities.hoverProvider = false
+						end,
 						init_options = {
 							settings = {
 								-- Any extra CLI arguments for `ruff` go here.
@@ -118,6 +119,132 @@ return {
 						},
 					})
 				end,
+			})
+		end,
+	},
+
+	{
+		"mfussenegger/nvim-dap",
+		config = function()
+			local dap = require("dap")
+			local function pytest_env()
+				if not vim.g.file_exists(".vscode/launch.json") then
+					return {}
+				end
+				local file = io.open(".vscode/launch.json", "r")
+				local configs = require("json"):decode(file:read("*all"))["configurations"]
+				for _, config in pairs(configs) do
+					if config["module"] == "pytest" then
+						return config["env"]
+					end
+				end
+			end
+
+			dap.adapters.dart = {
+				type = "executable",
+				command = "dart",
+				args = { "debug_adapter" },
+			}
+			dap.configurations.dart = {
+				{
+					type = "dart",
+					request = "launch",
+					name = "Launch Dart Program",
+					program = "${file}",
+					cwd = "${workspaceFolder}",
+					args = { "--help" },
+				},
+			}
+			dap.adapters.dart = {
+				type = "executable",
+				command = "flutter",
+				args = { "debug_adapter" },
+			}
+			dap.configurations.dart = {
+				{
+					type = "dart",
+					request = "launch",
+					name = "Launch Flutter Program",
+					program = "${file}",
+					cwd = "${workspaceFolder}",
+					toolArgs = { "-d", "ios" },
+				},
+			}
+			vim.keymap.set("n", "<F5>", function()
+				require("dap").continue()
+			end)
+			vim.keymap.set("n", "<F10>", function()
+				require("dap").step_over()
+			end)
+			vim.keymap.set("n", "<F11>", function()
+				require("dap").step_into()
+			end)
+			vim.keymap.set("n", "<F12>", function()
+				require("dap").step_out()
+			end)
+			vim.keymap.set("n", "<F9>", function()
+				require("dap").terminate()
+			end)
+			vim.keymap.set("n", "<Leader>b", function()
+				require("dap").toggle_breakpoint()
+			end)
+			vim.keymap.set("n", "<Leader>B", function()
+				require("dap").set_breakpoint()
+			end)
+			vim.keymap.set("n", "<Leader>lp", function()
+				require("dap").set_breakpoint(nil, nil, vim.fn.input("Log point message: "))
+			end)
+			vim.keymap.set("n", "<Leader>dr", function()
+				require("dap").repl.open()
+			end)
+			vim.keymap.set("n", "<Leader>dl", function()
+				require("dap").run_last()
+			end)
+			vim.keymap.set({ "n", "v" }, "<Leader>dh", function()
+				require("dap.ui.widgets").hover()
+			end)
+			vim.keymap.set({ "n", "v" }, "<Leader>dp", function()
+				require("dap.ui.widgets").preview()
+			end)
+			vim.keymap.set("n", "<Leader>df", function()
+				local widgets = require("dap.ui.widgets")
+				widgets.centered_float(widgets.frames)
+			end)
+			vim.keymap.set("n", "<Leader>ds", function()
+				local widgets = require("dap.ui.widgets")
+				widgets.centered_float(widgets.scopes)
+			end)
+
+			vim.keymap.set("n", "<leader>tf", function()
+				require("neotest").run.run({ vim.fn.expand("%"), strategy = "dap", env = pytest_env() })
+			end)
+		end,
+	},
+	{
+		"jay-babu/mason-nvim-dap.nvim",
+		dependencies = { "mfussenegger/nvim-dap", "kmontocam/nvim-conda" },
+		config = function()
+			require("mason-nvim-dap").setup({
+				ensure_installed = { "stylua", "debugpy" },
+				handlers = {
+					function(config)
+						-- all sources with no handler get passed here
+
+						-- Keep original functionality
+						require("mason-nvim-dap").default_setup(config)
+					end,
+					python = function(config)
+						config.adapters = {
+							type = "executable",
+							command = os.getenv("CONDA_PREFIX") .. "/bin/python",
+							args = {
+								"-m",
+								"debugpy.adapter",
+							},
+						}
+						require("mason-nvim-dap").default_setup(config)
+					end,
+				},
 			})
 		end,
 	},
