bfbd9462bb620131d931d618ca21e4e4b09be228
Tristan Sutton <tsutton@ORG5119413.OCN.ORIGIN.COM.AU>
2024-02-01 14:47:19 +1100

    harpoon mappings

--- lua/plugins/harpoon.lua
+++ lua/plugins/harpoon.lua
@@ -5,18 +5,13 @@ return {
     dependencies = { "nvim-lua/plenary.nvim" },
     config = function()
       local harpoon = require("harpoon")
-      harpoon.setup({
-        global_settings = {
-          save_on_toggle = true,
-          save_on_change = true,
-        },
-      })
+      harpoon.setup()
       vim.keymap.set("n", "<leader>a", function() harpoon:list():append() end)
       vim.keymap.set("n", "<C-e>", function() harpoon.ui:toggle_quick_menu(harpoon:list()) end)
 
       -- Toggle previous & next buffers stored within Harpoon list
-      vim.keymap.set("n", "<leader>o", function() harpoon:list():prev() end)
-      vim.keymap.set("n", "<leader>i", function() harpoon:list():next() end)
+      vim.keymap.set("n", "<C-h>", function() harpoon:list():prev() end)
+      vim.keymap.set("n", "<C-l>", function() harpoon:list():next() end)
     end,
   }
 }
