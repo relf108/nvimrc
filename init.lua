8885a188650adb04afa87992eba9cf3f3cec6561
Tristan Sutton <tsutton@ORG117451.OCN.ORIGIN.COM.AU>
2024-02-08 15:30:12 +1100

    Remove old git conflict proj

--- init.lua
+++ init.lua
@@ -101,7 +101,3 @@ end
 
 require("dap.ext.vscode").load_launchjs()
 
-vim.keymap.set("n", "<space>e", vim.diagnostic.open_float)
-vim.keymap.set("n", "[d", vim.diagnostic.goto_prev)
-vim.keymap.set("n", "]d", vim.diagnostic.goto_next)
-vim.keymap.set("n", "<space>q", vim.diagnostic.setloclist)
