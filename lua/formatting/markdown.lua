fb7f64653903355cc3c936531dd491b468d46642
Tristan Sutton <tristan.sutton@origin.com.au>
2024-03-12 11:32:38 +1100

    Markdown formatting

---
+++ lua/formatting/markdown.lua
@@ -0,0 +1,10 @@
+local job = require("plenary.job")
+local on_exit = require("formatting.utils.format_exit")
+
+return function()
+	return job:new({
+		command = "prettier -w",
+		args = { vim.api.nvim_buf_get_name(0) },
+		on_exit = on_exit,
+	})
+end
--- lua/formatting/utils/format.lua
+++ lua/formatting/utils/format.lua
@@ -1,11 +1,13 @@
 local sql = require("formatting.sql")
 local lua = require("formatting.lua")
+local markdown = require("formatting.markdown")
 
 local format_overrides = {
 	sql = sql,
 	plsql = sql,
 	mysql = sql,
 	lua = lua,
+	md = markdown,
 }
 
 return function()
