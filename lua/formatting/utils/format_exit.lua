9fd40a991cd0aff49a24ff116d8a7290ec48935b
Tristan Sutton <tristan.sutton@gmail.com>
2024-03-19 15:20:25 +1100

    Reduce clutter in formatting

--- lua/formatting/utils/format_exit.lua
+++ lua/formatting/utils/format_exit.lua
@@ -1,13 +1,12 @@
 return function(_, return_val)
-	vim.notify("Formatter overridden", vim.log.levels.INFO)
 	if return_val == 0 then
 		vim.schedule(function()
-			vim.notify("Formatted with exit code " .. return_val, vim.log.levels.INFO)
+			vim.notify("Formatter overridden: Exit code " .. return_val, vim.log.levels.INFO)
 			vim.cmd(":e!")
 		end)
 	else
 		vim.schedule(function()
-			vim.notify("Formatted with non-zero exit code, using LSP", vim.log.levels.WARN)
+			vim.notify("Formatter overridden: Non-zero exit code, using LSP", vim.log.levels.WARN)
 			vim.lsp.buf.format({ async = true })
 		end)
 	end
