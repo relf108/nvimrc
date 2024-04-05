89b28ab20615bd64399e20cec113271b995e9366
Tristan Sutton <tristan.sutton@gmail.com>
2024-04-05 12:31:43 +1100

    Update telescope config

--- lua/plugins/telescope.lua
+++ lua/plugins/telescope.lua
@@ -1,22 +1,48 @@
+-- plugins/telescope.lua:
 return {
 	{
 		"nvim-telescope/telescope.nvim",
 		branch = "0.1.x",
+		dependencies = { "nvim-lua/plenary.nvim" },
 		config = function()
+			local telescope = require("telescope")
+			local telescopeConfig = require("telescope.config")
 			local builtin = require("telescope.builtin")
+
+			-- Clone the default Telescope configuration
+			local vimgrep_arguments = { unpack(telescopeConfig.values.vimgrep_arguments) }
+
+			-- I want to search in hidden/dot files.
+			table.insert(vimgrep_arguments, "--hidden")
+			-- I don't want to search in the `.git` directory.
+			table.insert(vimgrep_arguments, "--glob")
+			table.insert(vimgrep_arguments, "!**/.git/*")
+
+			vim.keymap.set("n", "<leader>ff", builtin.find_files, {})
+			vim.keymap.set("n", "<leader>fg", builtin.live_grep, {})
 			vim.keymap.set("n", "<leader>fb", builtin.buffers, {})
-			vim.api.nvim_set_keymap(
-				"n",
-				"<Leader>ff",
-				':lua require"telescope.builtin".find_files({ hidden = true, file_ignore_patterns = {".git"} })<CR>',
-				{ noremap = true, silent = true }
-			)
-			vim.api.nvim_set_keymap(
-				"n",
-				"<Leader>fg",
-				':lua require"telescope.builtin".live_grep({ hidden = true, file_ignore_patterns = {".git"} })<CR>',
-				{ noremap = true, silent = true }
-			)
+			vim.keymap.set("n", "<leader>fh", builtin.help_tags, {})
+
+			telescope.setup({
+				defaults = {
+					-- `hidden = true` is not supported in text grep commands.
+					vimgrep_arguments = vimgrep_arguments,
+				},
+				pickers = {
+					find_files = {
+						-- `hidden = true` will still show the inside of `.git/` as it's not `.gitignore`d.
+						find_command = { "rg", "--files", "--hidden", "--glob", "!**/.git/*" },
+					},
+					live_grep = {
+						-- `hidden = true` will still show the inside of `.git/` as it's not `.gitignore`d.
+						find_command = { "rg", "--hidden", "--glob", "!**/.git/*" },
+					},
+					buffers = {
+						-- `hidden = true` will still show the inside of `.git/` as it's not `.gitignore`d.
+						find_command = { "rg" },
+					},
+				},
+			})
 		end,
 	},
 }
