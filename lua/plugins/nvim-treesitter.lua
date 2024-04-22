6ddc7af7c5d7184365b8b78ca62c2895f01e26ac
Sutton <tristan.sutton@origin.com.au>
2024-04-22 13:09:12 +1000

    Pkl support

--- lua/plugins/nvim-treesitter.lua
+++ lua/plugins/nvim-treesitter.lua
@@ -33,4 +33,15 @@ return {
 			})
 		end,
 	},
+	{
+		"https://github.com/apple/pkl-neovim",
+		lazy = true,
+		event = "BufReadPre *.pkl",
+		dependencies = {
+			"nvim-treesitter/nvim-treesitter",
+		},
+		build = function()
+			vim.cmd("TSInstall! pkl")
+		end,
+	},
 }
