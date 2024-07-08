f1cc96fc439fefc0ea428c088ef5ded2f5032bf1
Tristan Sutton <tristan.sutton@gmail.com>
2024-07-08 12:04:25 +1000

    Set repl size to 20%

--- lua/plugins/dap-ui.lua
+++ lua/plugins/dap-ui.lua
@@ -60,11 +60,11 @@ return {
 						elements = {
 							{
 								id = "repl",
-								size = 0.5,
+								size = 0.2,
 							},
 							{
 								id = "console",
-								size = 0.5,
+								size = 0.8,
 							},
 						},
 						position = "bottom",
