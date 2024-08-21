b59da13fe4f366b5eaa02e5b53505520114605ad
Tristan Sutton <tristan.sutton@gmail.com>
2024-08-21 14:55:31 +1000

    Pyright

--- lua/lsp/python.lua
+++ lua/lsp/python.lua
@@ -1,19 +1,34 @@
-local capabilities = require("cmp_nvim_lsp").default_capabilities()
-local ruff_conf = vim.fn.expand("~/.config/ruff/ruff.toml")
-require("lspconfig").ruff_lsp.setup({
-	capabilities = capabilities,
-	on_attach = function(client)
-		client.server_capabilities.hoverProvider = false
-	end,
-	init_options = {
-		settings = {
-			args = {
-				"--config=" .. ruff_conf,
+-- local capabilities = require("cmp_nvim_lsp").default_capabilities()
+-- local ruff_conf = vim.fn.expand("~/.config/ruff/ruff.toml")
+-- require("lspconfig").ruff_lsp.setup({
+-- 	capabilities = capabilities,
+-- 	on_attach = function(client)
+-- 		client.server_capabilities.hoverProvider = false
+-- 	end,
+-- 	init_options = {
+-- 		settings = {
+-- 			args = {
+-- 				"--config=" .. ruff_conf,
+-- 			},
+-- 		},
+-- 	},
+-- })
+-- require("lspconfig").jedi_language_server.setup({
+-- 	capabilities = capabilities,
+-- })
+--
+require("lspconfig").pyright.setup({
+	capabilities = require("cmp_nvim_lsp").default_capabilities(),
+	settings = {
+		python = {
+			pythonPath = vim.g.python_path(),
+			analysis = {
+				autoSearchPaths = true,
+				useLibraryCodeForTypes = true,
+				diagnosticMode = "workspace",
+				typeCheckingMode = true,
 			},
 		},
 	},
-})
-require("lspconfig").jedi_language_server.setup({
-	capabilities = capabilities,
 })
 return
