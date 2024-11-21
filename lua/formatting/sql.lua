f6fb9c996eaba8f88843029247b73d916c99b880
Tristan Sutton <tristan.sutton@gmail.com>
2024-11-22 09:57:25 +1100

    sqlfluff reads pyproj config if exists

--- lua/formatting/sql.lua
+++ lua/formatting/sql.lua
@@ -2,10 +2,16 @@ local job = require("plenary.job")
 local on_exit = require("formatting.utils.format_exit")
 
 return function()
+	local args = { "fix", vim.g.formatting_buf_name, "--dialect", "postgres" }
+	if vim.g.file_exists("./pyproject.toml") then
+		table.insert(args, "--config")
+		table.insert(args, "./pyproject.toml")
+	end
+
 	return {
 		job:new({
 			command = "sqlfluff",
-			args = { "fix", vim.g.formatting_buf_name, "--dialect", "postgres" },
+			args = args,
 			on_exit = on_exit,
 		}),
 	}
