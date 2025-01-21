628d1a28ebbbf238106ea0f105033c7f082d56b0
Tristan Sutton <tristan.sutton@gmail.com>
2025-01-21 12:18:02 +1100

    Fix sql formatting for db buffers

--- lua/formatting/sql.lua
+++ lua/formatting/sql.lua
@@ -1,8 +1,31 @@
 local job = require("plenary.job")
 local on_exit = require("formatting.utils.format_exit")
 
+local added_ext = false
+
+local function on_exit_override(_, return_val)
+	if added_ext then
+		local oldname = vim.g.formatting_buf_name:gsub("%.sql", "")
+		os.rename(vim.g.formatting_buf_name, oldname)
+		vim.g.formatting_buf_name = oldname
+		added_ext = false
+	end
+	on_exit(_, return_val)
+end
+
 return function()
-	local args = { "fix", vim.g.formatting_buf_name, "--dialect", "postgres" }
+	if
+		not string.match(vim.g.formatting_buf_name, "%.sql")
+		and not string.match(vim.g.formatting_buf_name, "%.plsql")
+		and not string.match(vim.g.formatting_buf_name, "%.mysql")
+	then
+		local newname = vim.g.formatting_buf_name .. ".sql"
+		os.execute("cp " .. vim.g.formatting_buf_name .. " " .. newname)
+		vim.g.formatting_buf_name = newname
+		added_ext = true
+	end
+
+	local args = { "fix", vim.g.formatting_buf_name, "--dialect", "postgres", "-q" }
 	if vim.g.file_exists("./pyproject.toml") then
 		table.insert(args, "--config")
 		table.insert(args, "./pyproject.toml")
@@ -12,7 +35,7 @@ return function()
 		job:new({
 			command = "sqlfluff",
 			args = args,
-			on_exit = on_exit,
+			on_exit = on_exit_override,
 		}),
 	}
 end
