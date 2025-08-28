6234b0d52db101d023634527ba0b848ab48b5782
Tristan Sutton <tristan.sutton@gmail.com>
2025-08-28 11:18:04 +1000

    feat: Move to nvim-unstack

--- lua/plugins/nvim-unstack.lua
+++ lua/plugins/nvim-unstack.lua
@@ -1,3 +1,11 @@
 return {
-	dir = "~/Projects/nvim-unstack",
+	"relf108/nvim-unstack",
+	event = "VeryLazy",
+	version = "*",
+	opts = {
+		debug = false, -- Disable debug logging for performance
+		showsigns = true, -- Disable signs to reduce visual overhead
+		layout = "tab", -- Use tab layout (default, but explicit for clarity)
+		mapkey = "<leader>s", -- Keep default mapping
+	},
 }
