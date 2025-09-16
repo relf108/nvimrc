02fc283526c50254795f5adfaf2bec77f6d4917e
Tristan Sutton <tristan.sutton@gmail.com>
2025-09-16 11:18:31 +1000

    fix: sql formatting working in dadbod

--- lua/formatting/sql.lua
+++ lua/formatting/sql.lua
@@ -2,13 +2,13 @@ local job = require("plenary.job")
 local on_exit = require("formatting.utils.format_exit")
 
 local added_ext = false
+local name = ""
 
 local function on_exit_override(_, return_val)
 	if added_ext then
-		local oldname = vim.g.formatting_buf_name:gsub("%.sql", "")
-		os.rename(vim.g.formatting_buf_name, oldname)
-		vim.g.formatting_buf_name = oldname
+		os.rename(name, vim.g.formatting_buf_name)
 		added_ext = false
+		name = ""
 	end
 	on_exit(_, return_val)
 end
@@ -19,13 +19,16 @@ return function()
 		and not string.match(vim.g.formatting_buf_name, "%.plsql")
 		and not string.match(vim.g.formatting_buf_name, "%.mysql")
 	then
-		local newname = vim.g.formatting_buf_name .. ".sql"
-		os.execute("cp " .. vim.g.formatting_buf_name .. " " .. newname)
-		vim.g.formatting_buf_name = newname
+		name = vim.g.formatting_buf_name .. ".sql"
+		os.execute("cp " .. vim.g.formatting_buf_name .. " " .. name)
 		added_ext = true
 	end
 
-	local args = { "fix", vim.g.formatting_buf_name, "--dialect", "postgres", "-q" }
+	if not added_ext then
+		name = vim.g.formatting_buf_name
+	end
+
+	local args = { "fix", name, "--dialect", "postgres", "-q" }
 	if vim.g.file_exists("./pyproject.toml") then
 		table.insert(args, "--config")
 		table.insert(args, "./pyproject.toml")
--- lua/plugins/fastaction.lua
+++ lua/plugins/fastaction.lua
@@ -2,7 +2,6 @@ return {
 	"Chaitanyabsprip/fastaction.nvim",
 	---@type FastActionConfig
 	opts = {
-		lazy = false,
 		register_ui_select = true,
 	},
 }
